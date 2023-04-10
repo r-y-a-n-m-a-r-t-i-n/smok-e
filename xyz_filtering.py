60152580a69416899b5fdcc48b50e2787fe0aff0
r-y-a-n-m-a-r-t-i-n <ryanlmartin2k@gmail.com>
2023-04-10 13:15:20 -0400

    working on implementing arc path planning

--- xyz_filtering.py
+++ xyz_filtering.py
@@ -34,6 +34,37 @@ def find_arm_trajectory(r_des, q_guess):
     return sol
 
 
+# from https://kitchingroup.cheme.cmu.edu/blog/2015/01/18/Equation-of-a-plane-through-three-points/
+def calcPlane(p1, p2, p3):
+    # These two vectors are in the plane
+    v1 = p3 - p1
+    v2 = p2 - p1
+
+    # the cross product is a vector normal to the plane
+    cp = np.cross(v1, v2)
+    a, b, c = cp
+
+    # This evaluates a * x3 + b * y3 + c * z3 which equals d
+    d = np.dot(cp, p3)
+
+    print('The equation is {0}x + {1}y + {2}z = {3}'.format(a, b, c, d))
+    return np.array((a, b, c, d))
+
+
+# from https://math.stackexchange.com/questions/3372513/calculate-the-coordinates-of-points-on-a-circle-in-3d-space
+def calcCircle(a, b, c, d, z, r):
+    # need to figure out how to select between plus and minus - somehow depends on relative positions of start/goal
+    x_num = -a * c * z + np.sqrt(b ** 2 * (r ** 2 * (a ** 2 + b ** 2) - z ** 2 * (a ** 2 + b ** 2 + c ** 2)))
+    x_den = a ** 2 + b ** 2
+    x = x_num / x_den
+
+    y_num = -b ** 2 * c * z + a * np.sqrt(b ** 2 * (r ** 2 * (a ** 2 + b ** 2) - z ** 2(a ** 2 + b ** 2 + c ** 2)))
+    y_den = b * (a ** 2 + b ** 2)
+    y = y_num / y_den
+
+    return x, y, z
+
+
 def main(currPos: np.ndarray, finPos: np.ndarray):
     # where am I now
     r_curr = currPos
@@ -44,6 +75,12 @@ def main(currPos: np.ndarray, finPos: np.ndarray):
     # TODO: intermediate positions between start and end - arc?
     # the linspace is just a line, change to arc with start and end on circle and servo 1 as center
     xyz_positions = np.linspace(r_curr, r_des)
+
+    a, b, c, d = calcPlane(r_curr, r_des, np.array((0, 0, 0)))
+    r = np.linalg.norm(r_curr)
+
+    for i in range(len(xyz_positions)):
+        xyz_positions[i] = calcCircle(a, b, c, d, xyz_positions[i][2], r)
 
     # guess angle positions for solver
     q_guesses = np.empty((len(xyz_positions), 3))
